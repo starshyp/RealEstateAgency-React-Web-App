4ffd0f611b54d9b2ad771f07d205a8e3fd685d7f
CB <ybloc1@outlook.com>
2021-08-14 20:30:02 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Index from './main-page';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/main-page/index.js
@@ -0,0 +1,54 @@
+// import logo from './logo.svg';
+import {useEffect, useState, useMemo} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import './main-page.css';
+import Header from "./header"
+import FeaturedHouse from "./featured-house";
+import SearchResults from "../search-results";
+import HouseFilter from "./house-filter";
+import HouseFromQuery from "../house/HouseFromQuery";
+
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+  return (
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+        <HouseFilter allHouses={allHouses} />
+        <Switch>
+          <Route path="/searchresults/:country">
+            <SearchResults allHouses={allHouses}/>
+          </Route>
+          <Route path="/house/:id">
+            <HouseFromQuery allHouses={allHouses}/>
+          </Route>
+          <Route path="/">
+            <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // <div className="container">
+    //   <Header subtitle="Providing houses all over the world"/>
+    // </div>
+  );
+}
+
+export default App;
